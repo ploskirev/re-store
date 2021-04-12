23518b1259a5164a9d52c09e6f504f654a4956cf
Denis Petrov-Ploskirev <44771152+ploskirev@users.noreply.github.com>
2021-04-12 19:25:54 +0300

    initial

---
+++ src/components/app/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './style.css'
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
---
+++ src/components/spinner/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './style.css'
+
+const App = () => {
+  return (
+    <div>loading..</div>
+  )
+}
+
+export default App
